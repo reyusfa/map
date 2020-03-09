cd17b45e604a9eb7b894bc1e811462280eac1627
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-03-09 17:12:44 +0700

    styling login page and header

--- src/Auth/Login/Login.js
+++ src/Auth/Login/Login.js
@@ -1,20 +1,58 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Button, Input} from 'react-native-elements';
+import {ScrollView, View, StyleSheet} from 'react-native';
+import {Button, Input, Icon} from 'react-native-elements';
 
 const Login = props => {
   const {navigation} = props;
   navigation.setOptions({
     title: 'SkyJack',
+    headerLeft: () => (
+      <Icon
+        name="menu"
+        type="feather"
+        containerStyle={styles.leftHeaderIcon}
+        onPress={() => {
+          navigation.openDrawer();
+        }}
+      />
+    ),
+    headerRight: () => (
+      <Icon
+        name="more-vertical"
+        type="feather"
+        containerStyle={styles.rightHeaderIcon}
+      />
+    ),
   });
 
   return (
-    <View>
-      <Input placeholder="Email/Username/PhoneNumber" />
-      <Input placeholder="Password" secureTextEntry />
-      <Button title="Login" />
-    </View>
+    <ScrollView>
+      <View style={styles.inputContainer}>
+        <Input placeholder="Email/Username/PhoneNumber" />
+        <Input placeholder="Password" secureTextEntry />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button raised title="Login" containerStyle={styles.button} />
+      </View>
+    </ScrollView>
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 90,
+  },
+  buttonContainer: {
+    flex: 1,
+    marginTop: 30,
+  },
+  button: {
+    width: 125,
+    alignSelf: 'center',
+  },
+  leftHeaderIcon: {margin: 16, alignSelf: 'center'},
+  rightHeaderIcon: {margin: 16, alignSelf: 'center'},
+});
