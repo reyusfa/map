765a98a31089a3cfe97f385501059b106a965065
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-03-09 18:08:24 +0700

    map page mockup

--- src/App/Maps/Maps.js
+++ src/App/Maps/Maps.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Button, Input} from 'react-native-elements';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, Input, Icon} from 'react-native-elements';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const Maps = props => {
@@ -10,18 +10,67 @@ const Maps = props => {
   });
 
   return (
-    <View>
-      <Input placeholder="Location" />
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -7.5591225,
-          longitude: 110.7837924,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
+    <View style={styles.flex1}>
+      <View style={styles.flex1}>
+        <View
+          style={{
+            zIndex: 10,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              borderWidth: 1,
+              borderColor: '#dddddd',
+              backgroundColor: 'white',
+            }}>
+            <Icon
+              type="feather"
+              name="menu"
+              containerStyle={{
+                paddingVertical: 16,
+                paddingLeft: 16,
+              }}
+            />
+            <Input placeholder="Location" />
+          </View>
+        </View>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.flex1}
+          initialRegion={{
+            latitude: -7.5591225,
+            longitude: 110.7837924,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        />
+        <Icon
+          containerStyle={{bottom: 120, right: 20, position: 'absolute'}}
+          type="material"
+          name="my-location"
+          reverse
+          reverseColor="dodgerblue"
+          color="white"
+          raised
+        />
+        <Icon
+          containerStyle={{bottom: 50, right: 20, position: 'absolute'}}
+          type="material"
+          name="directions"
+          reverse
+          color="dodgerblue"
+          raised
+        />
+      </View>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        style={styles.locationButtonContainer}>
+        <Icon type="material" color="dodgerblue" name="place" />
+        <Text style={styles.locationButtonLabel}>Location</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -29,7 +78,19 @@ const Maps = props => {
 export default Maps;
 
 const styles = StyleSheet.create({
-  map: {
+  flex1: {
     flex: 1,
+  },
+  locationButtonContainer: {
+    flex: 0.1,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#dddddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  locationButtonLabel: {
+    fontSize: 16,
+    color: 'dodgerblue',
   },
 });
